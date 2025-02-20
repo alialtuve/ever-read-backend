e943f7127cbd02c826575c08eedff8901a7db85f
alialtuve <altuve.ali@gmail.com>
2025-02-20 12:34:32 -0600

    updated get all lends controller for pagination and search options

--- controllers/lend.controller.js
+++ controllers/lend.controller.js
@@ -41,7 +41,91 @@ const createLend = async(req, res) => {
 }
 
 const getAllLend = async(req, res) => {
-  const lend = await Lend.find({'returned':false})
+  
+  const { title, user  } = req.query;  
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page -1 ) * limit;  
+  
+  let lend = ''
+  let searchOptions = {};
+  let total=0;
+
+  if(user) {
+    searchOptions = { 'user.name': {$regex: user}}
+  }
+
+  if(title) {
+    searchOptions = { 'book.title': {$regex: title}}
+  }  
+
+  if(user || title ){
+    
+    lend = await Lend.aggregate([
+        {
+          $lookup:{
+            from:'books',
+            localField:'book',
+            foreignField:'_id',
+            as:'book',
+            pipeline:[
+              {
+                $lookup:{
+                  from: 'authors',
+                  localField:'author',
+                  foreignField:'_id',
+                  as:'author'
+                }
+              }
+            ]
+          }
+        },
+        {
+          $lookup:{
+            from:'users',
+            localField:'user',
+            foreignField:'_id',
+            as:'user'
+          }
+        },
+        {
+          $set:
+          {
+            user:{
+              $map:{
+                input:'$user',
+                in:{_id:'$$this._id', name:'$$this.name', lastName:'$$this.lastName', email:'$$this.email'}
+              }
+            },
+            book:{
+              $map:{
+                input:'$book',
+                in:{
+                  _id:'$$this._id', 
+                  title:'$$this.title',
+                  author: {
+                    _id:'$$this.author._id',
+                    name: '$$this.author.name'
+                  }
+                }
+              },
+            }
+          }
+        },
+        { $unwind:"$book"},
+        { $unwind:"$user"},
+        { $unwind: "$book.author._id"},
+        { $unwind: "$book.author.name"},
+        { $match:{'returned': false}},
+        { $match: searchOptions},
+        { $skip: skip},
+        { $limit: limit}
+      ]) 
+
+      
+  } else {
+    
+    lend = await Lend.find({'returned': false})
                 .populate({ 
                   path:'book', 
                   model:Book, 
@@ -55,10 +139,17 @@ const getAllLend = async(req, res) => {
                 .populate({ 
                   path:'user', 
                   model:User, 
-                  select: { 'name':1, 'email':1 }
-                });
+                  select: { 'name':1,'lastName':1, 'email':1 }
+                })
+                .skip(skip)
+                .limit(limit)
 
-  res.status(StatusCodes.OK).json({ lend, total: lend.length });
+    }
+
+  total = await Lend.countDocuments({'returned': false});
+  const numOfPages = Math.ceil(total/limit);
+  
+  res.status(StatusCodes.OK).json({ lend, total, numOfPages, currentPage: page });
 }
 
 const getLend = async(req, res) => {
@@ -106,7 +197,6 @@ const updateLend = async(req, res) => {
   );
   
   if(lend) {
-    console.log('lend', lend);
     availability = await checkAvalability(lend.book);
 
     if(availability === 0){
